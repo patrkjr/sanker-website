2e914c20dbb0b433e212380b16e614af0da96d6d
Patrick Kjær <patrick.jessen@icloud.com>
2025-06-29 19:53:02 +0200

    New landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
+import typographyPlugin from '@tailwindcss/typography';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import plugin from 'tailwindcss/plugin';
-import typographyPlugin from '@tailwindcss/typography';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
@@ -12,11 +12,32 @@ export default {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        green: {
+          '50': '#EEFFF0',
+          '100': '#D7FFDE',
+          '200': '#B2FFC0',
+          '300': '#68D391',
+          '400': '#48BB78',
+          '500': '#38A169',
+          '600': '#2F855A',
+          '700': '#276749',
+          '800': '#22543D',
+          '900': '#1C4532',
+          '950': '#0F2E1C',
+        },
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+
+      fontWeight: {
+        DEFAULT: '600', // semibold
+      },
+
+      letterSpacing: {
+        DEFAULT: '0.1em', // slightly more spacing than default
       },
 
       animation: {
